4588b39cfe8d0b45b14046172621ed32d2737be5
Khurshidbek Bakhromjonov <khurshidbekjava@gmail.com>
2023-05-22 13:07:23 +0500

    finalizing CurrenciesOfCurrentTestCase

---
+++ jsp-currencies/src/main/java/com/epam/rd/jsp/currencies/Launcher.java
@@ -0,0 +1,11 @@
+package com.epam.rd.jsp.currencies;
+
+import org.apache.catalina.LifecycleException;
+
+import javax.servlet.ServletException;
+
+public class Launcher {
+    public static void main(String[] args) throws ServletException, LifecycleException, InterruptedException {
+
+    }
+}
